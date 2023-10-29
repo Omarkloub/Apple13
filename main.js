b8cd92b53991d57f8c58ce295e0135863dd34938
Omar kloub <136655508+Omarkloub@users.noreply.github.com>
2023-10-29 09:33:00 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let img =document.querySelector(".imagM");
+function phones (phone){
+    img.src = phone ;
+
+}
+let contenier = document.querySelector(".contenier");
+function colors (color){
+    contenier.style.background = color ;
+}
